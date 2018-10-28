77c4d6568289a7c7017111e42edf6ac49b21b87b
rvz0r <szymon.komosinski98@gmail.com>
2018-10-28 15:41:33 +0100

    v.0.0000003

--- src/main/java/Residents.list/PESEL_validation.java
+++ src/main/java/Residents.list/PESEL_validation.java
@@ -1,34 +1,37 @@
 package Residents.list;
 
-public class PESEL_validation {
-  public boolean validator(long x) {
-    long k = x%10;
+class PESEL_validation {
+  boolean validator(long x) {
+   /* long k = x%10;//1
     x = x/10;
-    long j = x%10;
+    long j = x%10;//2
     x = x/10;
-    long i = x%10;
+    long i = x%10;//3
     x = x/10;
-    long h = x%10;
+    long h = x%10;//4
     x = x/10;
-    long g = x%10;
+    long g = x%10;//5
     x = x/10;
-    long f = x%10;
+    long f = x%10;//6
     x = x/10;
-    long e = x%10;
+    long e = x%10;//7
     x = x/10;
-    long d = x%10;
+    long d = x%10;//8
     x = x/10;
-    long c = x%10;
+    long c = x%10;//9
     x = x/10;
-    long b = x%10;
+    long b = x%10;//10
     x = x/10;
-    long a = x%10;
-    long val = a + 3*b + 7*c + 9*d + 1*e + 3*f + 7*g + 9*h + i + 3*j + k;
-    if (val%10 == 0) {
-      return true;
+    long a = x%10;//11
+*/
+    long[] num;
+    num = new long[11];
+    for (int i1 = 0;i1<11;i1++){
+      num[i1] = x%10;
+      x = x/10;
     }
-    else {
-    return false;
-    }
+
+    long val = num[0] + 3*num[1] + 7*num[2] + 9*num[3] + num[4] + 3*num[5] + 7*num[6] + 9*num[7] + num[8] + 3*num[9] + num[10];
+    return val % 10 == 0;
   }
 }
